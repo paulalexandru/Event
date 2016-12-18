deaad743b3d1c031c107b28c4b801015924cf40c
paulalexandru <paul.ilea02@gmail.com>
2016-12-19 01:01:50 +0200

    Delegate solution

--- all.js
+++ all.js
@@ -1,6 +1,11 @@
-(function(){
-	if (document.getElementById('btn1')){
-		document.getElementById("btn1").addEventListener("click", displayMessage);
+(function(){		
+	if (document.getElementById('myParent')){		
+		
+		document.getElementById("myParent").addEventListener("click",function(e) {						
+			if (e.target && e.target.matches("button#btn1")) {
+				displayMessage();
+			}
+		});		
 	}
 	
 	function displayMessage(){
